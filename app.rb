47a31c760ca169ddb1bb87b572bf8aea8bd10acb
Erin Green <erin.y.green@gmail.com>
2018-12-20 20:07:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,14 @@ class App < Sinatra::Base
     erb :index
   end
   
+  get '/create_puppy' do 
+    erb :create_puppy
+  end
   
+  post '/display_puppy' do 
+    
+    erb :display_puppy
+    
+  end
 
 end
