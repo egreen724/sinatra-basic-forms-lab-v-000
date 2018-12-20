ca339b21d46e08d1f4b8ab8586f970cc61687a31
Erin Green <erin.y.green@gmail.com>
2018-12-20 20:10:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   
   post '/display_puppy' do 
     
-    new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
     
     erb :display_puppy
     
